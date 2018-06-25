f8c944063670368f1c06e8cd9555d6c5a3d29c34
Panin-arthur <40277610+Panin-arthur@users.noreply.github.com>
2018-06-25 10:31:26 +0300

    123
    123

---
+++ Form15_3/Form15.3+/Form15.3/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Form15._3
+{
+    partial class Form15_3
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.sfd = new System.Windows.Forms.SaveFileDialog();
+            this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.DoIt = new System.Windows.Forms.Button();
+            this.Outbox = new System.Windows.Forms.TextBox();
+            this.Input = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // ofd
+            // 
+            this.ofd.FileName = "openFileDialog1";
+            // 
+            // DoIt
+            // 
+            this.DoIt.Location = new System.Drawing.Point(57, 82);
+            this.DoIt.Name = "DoIt";
+            this.DoIt.Size = new System.Drawing.Size(75, 23);
+            this.DoIt.TabIndex = 2;
+            this.DoIt.Text = "Го";
+            this.DoIt.UseVisualStyleBackColor = true;
+            this.DoIt.Click += new System.EventHandler(this.DoIt_Click);
+            // 
+            // Outbox
+            // 
+            this.Outbox.Location = new System.Drawing.Point(173, 8);
+            this.Outbox.Multiline = true;
+            this.Outbox.Name = "Outbox";
+            this.Outbox.Size = new System.Drawing.Size(38, 252);
+            this.Outbox.TabIndex = 3;
+            // 
+            // Input
+            // 
+            this.Input.Location = new System.Drawing.Point(57, 111);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(75, 20);
+            this.Input.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(38, 8);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 28);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Интервал от -10 до 10";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(19, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Сформировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form15_3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(233, 260);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.Outbox);
+            this.Controls.Add(this.DoIt);
+            this.Name = "Form15_3";
+            this.Text = "Form15.3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SaveFileDialog sfd;
+        private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.Button DoIt;
+        private System.Windows.Forms.TextBox Outbox;
+        private System.Windows.Forms.TextBox Input;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
