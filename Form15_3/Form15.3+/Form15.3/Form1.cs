4ce22d0555fb043d1915b63dd281fe077b2a6e2e
Panin-arthur <40277610+Panin-arthur@users.noreply.github.com>
2018-06-25 10:37:05 +0300

    Add files via upload

---
+++ Form15_3/Form15.3+/Form15.3/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form15._3
+{
+    public partial class Form15_3 : Form
+    {
+        public Form15_3()
+        {
+            InitializeComponent();
+        }
+        private void DoIt_Click(object sender, EventArgs e)
+        {
+            RemoveLastNeg(queue);
+            Outbox.Lines = queue.Printer();
+        }
+
+        MyQueue queue = new MyQueue();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            queue = GenerateQueue(-10, 10, 10);
+            Outbox.Lines = queue.Printer();
+        }
+
+        //MyQueue GenerateQueue(int a, int n)
+        //{
+        //MyQueue q = new MyQueue();
+        //генератор
+        //  return q;
+        //}
+
+        MyQueue GenerateQueue(int a, int b, int n) //Создаём очередь
+        {
+            MyQueue q = new MyQueue();
+            var rnd = new Random();
+            int i = 0;
+            while (i < n)
+            {
+                q.InQueue(rnd.Next(a, b).ToString());
+                i++;
+            }
+            return q;
+        }
+
+        //void RemoveLastNeg(MyQueue q)
+        //{
+        //вынимаем и вставляем
+        //}
+
+        void RemoveLastNeg(MyQueue q)
+        {
+            MyQueue tmp = new MyQueue();
+            int idx = -1, qc = q.count;
+            for (int i = 0; i < qc; i++)
+            {
+                string s = q.OutQueue();
+                if (int.Parse(s) < 0) idx = i;
+                tmp.InQueue(s);
+            }
+            if (idx != -1)
+                for (int i = 0; i < qc; i++)
+                {
+                    string s = tmp.OutQueue();
+                    if (idx == i) continue;
+                    q.InQueue(s);
+                }
+        }
+    }
+}
---
+++ Form15_3/Form15.3+/Form15.3/Queue.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Form15._3
+{
+    class Node     // Узел для списка, стека, очереди
+    {
+        public string inf;
+        public Node next;
+        public Node(string inf, Node next)            // Конструктор
+        {
+            this.inf = inf;
+            this.next = next;
+        }
+    }
+    class MyQueue    // Класс Очередь
+    {
+        public Node head;                            // голова 
+        public Node tail;
+        public int count;                       // число элементов
+
+        public MyQueue()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            count = 0;
+        }
+
+        public bool QueueIsEmpty()              // проверка на пустоту
+        {
+            return head == null;
+        }
+
+        public void InQueue(string inf)            // положить в хвост очереди
+        {
+            Node p = new Node(inf, null);
+            if (QueueIsEmpty())
+            {
+                head = p;
+                tail = p;
+            }
+            else
+            {
+                tail.next = p;
+                tail = p;
+            }
+            count++;
+        }
+
+        public string OutQueue()                   // взять из головы очереди
+        {
+            Node p = head;
+            head = head.next;
+            count--;
+            return p.inf;
+        }
+
+        public string[] Printer()
+        {
+            int L = 0;
+            string[] st = new string[0];
+            Node p = head;
+            while (p != null)
+            {
+                Array.Resize(ref st, ++L);
+                st[L - 1] = p.inf.ToString();
+                p = p.next;
+            }
+            return st;
+        }
+
+    }
+}
